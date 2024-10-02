5ad544b36c1dbe02ea6fb57f8b227ec029fc01e7
Antonio-Milella <antonio.milella@marconibari.edu.it>
2024-10-02 09:32:47 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
+
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float media, promossi;
+    int n,i;
+    
+    i = 0;
+    promossi = 0;
+    cout << "Inserisci il numero di studenti presenti nella classe" << endl;
+    do {
+        cin >> n;
+        if (n <= 0) {
+            cout << "Devi inserire un numero maggiore di 0" << endl;
+        }
+    } while (n <= 0);
+    string nome[n];
+    int voto[n];
+    
+    media = 0;
+    for (i = 0; i <= n - 1; i++) {
+        cout << "Nome?" << endl;
+        cin >> nome[i];
+        cout << "Qual è il tuo voto medio in informatica?" << endl;
+        cin >> voto[i];
+        media = media + voto[i];
+        if (voto[i] >= 6) {
+            cout << "Il ragazzo di nome: " << nome[i] << " è promosso con voto: " << voto[i] << endl;
+            promossi = promossi + 1;
+        } else {
+            cout << "Il ragazzo di nome: " << nome[i] << " è stato bocciato con voto: " << voto[i] << endl;
+        }
+    }
+    cout << "Sono stati promossi ben " << promossi << " alunni su " << n << ". Quindi sono stati bocciati ben " << n - promossi << " alunni." << endl;
+    return 0;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
